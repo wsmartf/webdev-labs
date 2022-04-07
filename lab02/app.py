40311f5ed6cfbbf16921cb21810f2819c9ad80c1
wsmartf <wjsmart12@gmail.com>
2022-04-07 16:39:22 -0500

    lab02 complete

--- lab02/app.py
+++ lab02/app.py
@@ -30,7 +30,7 @@ quotes = (
 ##############
 @app.route('/')
 def exercise1():
-    return 'Hello World!'
+    return 'Hi, Erick Rubi!'
 
 
 ##############
@@ -40,6 +40,7 @@ def exercise1():
 def exercise2():
     return render_template(
         'quote-of-the-day.html',
+        quote=random.choice(quotes),
         user=current_user
     )
 
@@ -49,12 +50,17 @@ def exercise2():
 @app.route('/restaurant-data/')
 @app.route('/restaurant-data')
 def exercise3():
-    search_term = 'pizza'
-    location = 'Evanston, Il'
+    args = request.args
+    location = args.get('location')
+    search_term = args.get('term')
+    if not (location and search_term):
+        return '"location" and "term" are required query parameters'
+    
     url = 'https://www.apitutor.org/yelp/simple/v3/businesses/search?location={0}&term={1}'.format(location, search_term)
     response = requests.get(url)
     data = response.json()
     pprint(data) # for debugging -- prints the result to the command line
+
     return json.dumps(data)
 
 ##############
